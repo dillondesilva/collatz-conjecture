f8ccdd2d7694fb260c0d65d19400a2fec611b38b
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-12-29 22:50:21 +1100

    Curves up

---
+++ curves.js
@@ -0,0 +1,33 @@
+// n is the starting number of the collatz conjecture graph
+var n = 1000;
+var s = 1;
+function setup() {
+    createCanvas(800, 800);
+    noFill(0);
+    stroke(0);
+    background(255);
+    frameRate(30);
+    push();
+
+    for (k=0; k<n; k++) {
+        var collatzChain = getCollatzChain(k);
+        curve.apply(null, collatzChain);
+    }
+} 
+
+function getCollatzChain(n) {
+    var chain = [n];
+    var x = n;
+    var y = n;
+    while (x !== 1) {
+        if (x%2 === 0) {
+            y = x/2;
+        } else {
+            y = (3 * x) + 1;
+        }
+        chain.push(y);
+        x = y;
+    } 
+
+    return chain;
+}
