98a486ec4d773832fd6b7001e18bd1f747882a47
Dillon de Silva <dillon.desilva@geeksdiary.com>
2019-12-29 21:03:58 +1100

    Collatz Chain Generation Function

--- main.js
+++ main.js
@@ -1,28 +1,43 @@
 // n is the starting number of the collatz conjecture graph
 var n = 1000;
-var s = 0.9;
+var s = 1;
 function setup() {
-    createCanvas(1080, 1920);
+    createCanvas(800, 800);
     fill(0);
     stroke(0);
     background(255);
     frameRate(30);
     push();
 
-    for (k=1; k<n; k++) {
+    for (k=n-1; k<n; k++) {
         console.log(k);
         var x = k;
-        var y = k;
-        var old_x = k;
+        var y = getFirstCoord(k);
+        var old_y = k;
         while (x !== 1) {
             if (x%2 === 0) {
                 y = x/2;
             } else {
                 y = (3 * x) + 1;
             }
-            line(old_x * s, x * s, x * s, y * s);
-            old_x = x;
+            line(x * s, y * s, y * s, y * s);
+            // ellipse(k,y, 1)
             x = y;
         } 
     }
+}
+
+function getCollatzChain(n) {
+    var chain = [];
+
+    while (x !== 1) {
+        if (x%2 === 0) {
+            y = x/2;
+        } else {
+            y = (3 * x) + 1;
+        }
+        chain.push(y);
+        x = y;
+    } 
+
 }
